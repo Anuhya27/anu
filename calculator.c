871755ba9ae90f14d676feedf8256201aca54a88
Anuhya27 <anuhya17272gmail.com>
2019-10-19 06:41:02 -0700

    trying to merge

--- calculator.c
+++ calculator.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-int sum (int a, int b)
-{	return a+b;
+int diff(int a,int b)
+{	return a-b;
 }
 int main ( )
 	int a = 5; int b = 10;
 	printf("sum = %d ", sum(a,b));
+	printf("diff = %d", diff(a,b));
 	return 0;
 }
